cb8b4c112a2abc6eaea3ee2bea798fcc467a670f
Buzz <buzz@Bilals-MacBook-Pro.local>
2015-11-14 23:13:22 +1300

    Cube rotating in space

--- buzzdev/MainGameLoop.java
+++ buzzdev/MainGameLoop.java
@@ -1,7 +1,10 @@
 package buzzdev;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import buzzdev.entities.Camera;
+import buzzdev.entities.Entity;
 import buzzdev.models.RawModel;
 import buzzdev.models.TexturedModel;
 import buzzdev.render.DisplayManager;
@@ -16,38 +19,108 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();
 		
-		Loader loader = new Loader();
-		Render render = new Render();
-		StaticShader shader = new StaticShader();
+		
 		
 		//OpenGL expects vertices to be defined counter clockwise by default
-		float[] vertices = {
-				-0.5f, 0.5f, 0,
-				-0.5f, -0.5f, 0,
-				0.5f, -0.5f, 0,
-				0.5f, 0.5f, 0f
-		};
-		  
-		int[] indices = {
-				0,1,3,
-				3,1,2
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoord = {
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
 				0,0,
 				0,1,
 				1,1,
 				1,0
+
+				
 		};
 		
-		RawModel model = loader.loadToVAO(vertices, textureCoord, indices);
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
+		};
+		
+		Loader loader = new Loader();
+		StaticShader shader = new StaticShader();
+		Render render = new Render(shader);
+		
+		RawModel rawModel = loader.loadToVAO(vertices, textureCoord, indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("box"));
-		TexturedModel texturedModel = new TexturedModel(model, texture);
+		TexturedModel texturedModel = new TexturedModel(rawModel, texture);
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		
+		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
+			entity.increaseRotation(1, 1, 0);
+			camera.move();
 			render.prepare();
 			shader.start();
-			render.render(texturedModel);
+			
+			shader.loadViewMatrix(camera);
+			
+			render.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
